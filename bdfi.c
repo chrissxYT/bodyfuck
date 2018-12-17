e66cf342316668af6fcb8c42d01395d0be8ac75c
chrissx <chrissx@chrissx.ga>
2018-12-17 17:54:16 +0100

    Fixed the JP setter operators.

--- bdfi.c
+++ bdfi.c
@@ -25,13 +25,13 @@ void interpret(int op, FILE *i)
 		case '#': printf("%8x-%2x\n",tp,t[tp]);
 		case '*': printf("^I%x^\n", t[tp]);
 		case '1':jp=jp&0x00ffffffffffffff|t[tp];break;
-		case '2':jp=jp&0xff00ffffffffffff|t[tp];break;
-		case '3':jp=jp&0xffff00ffffffffff|t[tp];break;
-		case '4':jp=jp&0xffffff00ffffffff|t[tp];break;
-		case '5':jp=jp&0xffffffff00ffffff|t[tp];break;
-		case '6':jp=jp&0xffffffffff00ffff|t[tp];break;
-		case '7':jp=jp&0xffffffffffff00ff|t[tp];break;
-		case '8':jp=jp&0xffffffffffffff00|t[tp];break;
+		case '2':jp=jp&0xff00ffffffffffff|t[tp]>>8;break;
+		case '3':jp=jp&0xffff00ffffffffff|t[tp]>>16;break;
+		case '4':jp=jp&0xffffff00ffffffff|t[tp]>>24;break;
+		case '5':jp=jp&0xffffffff00ffffff|t[tp]>>32;break;
+		case '6':jp=jp&0xffffffffff00ffff|t[tp]>>40;break;
+		case '7':jp=jp&0xffffffffffff00ff|t[tp]>>48;break;
+		case '8':jp=jp&0xffffffffffffff00|t[tp]>>56;break;
 		case '=': call_jp(i); break;
 	}
 }
